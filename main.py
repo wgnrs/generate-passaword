8b4ef31aa9b4ac6a533ac55bc35d7b5b0bf5fe21
Wagner <wagner_silvajr@hotmail.com>
2023-02-24 11:56:50 -0300

    Implemented a validation on input and length of password

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from generate_password import generate_password
+from password_generator import generate_password
 
 
 def main():
-    length = int(input("Qual o comprimento da senha? "))
+    length = input("What's the length of password? ")
+
+    # validation if input is not a digit
+    if not length.isdigit():
+        print("You need put a number!")
+        return
+
+    length = int(length)
+
     password = generate_password(length)
     print(f"Sua senha gerada é: {password}")
+
 
 if __name__ == "__main__":
     main()
--- password_generator.py
+++ password_generator.py
@@ -7,6 +7,10 @@ def generate_password(length):
     letters = string.ascii_letters
     digits = string.digits
     possible_chars = letters + digits
+
+    if length < 8 or length > 20:
+        raise ValueError("Lenght of password must be between 8 and 20 characters")
+
     password = ''.join(random.choice(possible_chars) for i in range(length))
 
     return password
